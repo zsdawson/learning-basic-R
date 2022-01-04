7232b7fcd1ba9486443c9641b8458a6456cd643b
zsdawson <65880961+zsdawson@users.noreply.github.com>
2022-01-03 23:39:36 -0800

    Create 12-21-21.r

---
+++ Misc/12-21-21.r
@@ -0,0 +1,62 @@
+#for (x in 1:10) { 
+#the loop uses the first number to and including the last number
+#  print(x) # for loop testing 
+#}
+
+# var1 <- var2 <- var3 <- var4 <- 10
+# print(var1 + var2 + var3 + var4)
+# dog <- "maverick"
+
+# weight <- 55 
+# print(weight)
+
+# (weight <- 55)  
+
+
+# mass <- 47.5
+# age <- 122
+# mass <- mass * 2
+# age <- age - 20
+# mass_index <- mass/age
+
+# print(mass)
+# print(age)
+# print(mass_index)
+
+# weight <- 57.5
+# weight <-  2.2 * weight
+
+# weight <- 100
+# 
+# weight_kg <- sqrt(10)
+# weight_kg
+# round(weight_kg)
+# round(weight_kg, digits = 0)
+
+# weights <- c(100, 150, 250, 198.5, 25.2, 301.4) # vectors kind of like lists
+# print(weights)
+# 
+# length(weights) # a function that tells how many elements are in the variable 
+# class(weights)
+# weights <- c(weights, 20,21) # appending or adding to the vector (list) at the end
+# print(weights)
+# weights <- c(2000, weights) # appending or adding to the vector (list) at the start
+# print(weights)
+
+# num_char <- c(1, 2, 3)
+# print(num_char)
+# class(num_char)
+# animals <-  c("mouse", "rat", "dog", "cat")
+# animals[1] # taking item out of vector # vectors start at 1 
+# animals[c(3, 2)] # taking mutiple vector variables out of the vector 
+# 
+# more_amimals <- animals[c(1,2,3,2,1,4)] # maming nev vector with old vector variablles and items
+# more_amimals
+
+another <- c(21,34,49,32,99,43)
+another[c(TRUE,FALSE,FALSE,TRUE,TRUE,TRUE)] # it will print the True variables
+another
+another > 40 # will print t or f if over 40
+another[another > 40] # prints the variables that are over 40
+
+
