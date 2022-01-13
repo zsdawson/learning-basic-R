b20a99795cda03217534017d286e862a71e05ee7
zsdawson <65880961+zsdawson@users.noreply.github.com>
2022-01-13 09:04:10 -0800

    Update section-08.R

--- Udemy/section-08.R
+++ Udemy/section-08.R
@@ -6,3 +6,19 @@ matrix(v, nrow = 2)
 
 v2.1 <- matrix(v2,byrow = T, nrow = 4)
 v2.1
+
+################################################################################
+goog <- c(450,451,452,445,468)
+msft <- c(230,231,232,233,220) # just naming some vectors 
+ 
+stocks <- c(goog,msft)
+
+stockMatrix <- matrix(stocks,byrow=T,nrow = 2) #
+stockMatrix
+
+days <- c('Mon','Tue','Wed','Thu','fri') # names for  
+stNames <- c('goog','msft') #
+
+colnames(stockMatrix) <- days # naming the column 
+rownames(stockMatrix) <- stNames # naming the row
+stockMatrix
