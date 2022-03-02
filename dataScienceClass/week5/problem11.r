1619e75e4145741668ab0dfa0351a3b17a63225c
zsdawson <65880961+zsdawson@users.noreply.github.com>
2022-03-02 08:27:56 -0800

    Update problem11.r

--- dataScienceClass/week5/problem11.r
+++ dataScienceClass/week5/problem11.r
@@ -1,1 +1,14 @@
 
+library(tidyverse)
+myData = read_excel("/Users/zacharydawson/Desktop/Ch3_Q35_Data_File.xlsx")
+
+
+plot(myData$Writing ~myData$Math, type="n")
+symbols(myData$Writing ~myData$Math, circles = myData$`Test Takers`, inches = 0.25, bg='red', main = "Math/writing scores", xlab="Math Score", ylab="Writing Scores")
+
+ggplot(data = myData,
+       mapping = aes(x = Math,
+                     y = Writing,
+                     color = Type))+
+  geom_point(size = 2)
+
