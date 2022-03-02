7bb8ecfb32572d1a16ff8783dd8b2ea28b803e52
zsdawson <65880961+zsdawson@users.noreply.github.com>
2022-03-02 08:28:19 -0800

    Update problem13.r

--- dataScienceClass/week5/problem13.r
+++ dataScienceClass/week5/problem13.r
@@ -1,1 +1,11 @@
 
+library(tidyverse)
+
+myData = read_excel("/Users/zacharydawson/Desktop/Ch3_Q37_Data_File.xlsx")
+
+myData1 = myData[which(myData$Size == "M" & myData$Color == "Red"), ]
+n = table(myData1$Quantity)
+m = 1*n[1] + 2*n[2] + 3*n[3] + 4*n[4]
+
+
+
